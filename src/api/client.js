5b4c4ab868d4cd5fa74a7ab131ef7cca55619296
DmitriyZaulin <wanttodie37@gmail.com>
2026-05-22 00:24:33 +0300

    API-01: add base getting token to the local storage

--- src/api/client.js
+++ src/api/client.js
@@ -1,3 +1,28 @@
 import axios from "axios";
 
 
+export const API_BASE_URL = import.meta.env.API_BASE_URL;
+
+export const apiClient = axios.create({
+       baseURL: `${API_BASE_URL}/api/v1`,
+});
+
+apiClient.interceptors.request.use((config) =>{
+    const token = localStorage.getItem("access_token");
+    if(token){
+        config.headers.Authorization = `Beare ${token}`
+    }
+    return token;
+});
+
+
+apiClient.interceptors.response.user(
+    (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("access_token");
+    }
+
+    return Promise.reject(error);
+  }
+);
