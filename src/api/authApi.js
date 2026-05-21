9a351ef15b724af02e572894fc3d317a09ed81b8
DmitriyZaulin <wanttodie37@gmail.com>
2026-05-22 00:32:53 +0300

    AUTH-01: add base getting data functions from backend api

---
+++ src/api/authApi.js
@@ -0,0 +1,12 @@
+import { apiClient } from "./client";
+
+export async function  registerUser(data) {
+    const response = await apiClient.post("auth/register",data)
+    return response.data;
+}
+
+
+export async function loginUser(data) {
+    const response = await apiClient.post("auth/login",data)
+    return response.data
+}
