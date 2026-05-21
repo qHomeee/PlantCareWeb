41fa96c546858fcfaa524205b52598713e580940
DmitriyZaulin <wanttodie37@gmail.com>
2026-05-22 00:37:08 +0300

    API-02: add  getting functions from backend server info about user and updating it

---
+++ src/api/usersApi.js
@@ -0,0 +1,27 @@
+import { apiClient } from "./client";
+
+export async function getMe() {
+    const response = await apiClient.get("/user/me");
+
+    return response.data;
+}
+
+
+export async function updateMe(data) {
+    const response = await apiClient.patch("/users/me", data);
+    return response.data;
+}
+
+
+export async function uploadAvatar(file) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const response = await apiClient.post("/users/me/avatar", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return response.data;
+}
