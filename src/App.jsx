c188419cffc0eab45e640762db56a19cbf35acb7
DmitriyZaulin <wanttodie37@gmail.com>
2026-05-22 14:02:17 +0300

    STYLES-09: add care page css and connect all css files to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import AppRouter from "./router/AppRouter";
+
 import "./styles/global.css";
+import "./styles/layout.css";
+import "./styles/auth.css";
+import "./styles/profile.css";
+import "./styles/recognize.css";
+import "./styles/gallery.css";
+import "./styles/care.css";
 
 export default function App() {
   return <AppRouter />;
